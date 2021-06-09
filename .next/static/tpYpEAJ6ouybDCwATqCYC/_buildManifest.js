89a2195b8d3d22a6c261133ce581630ca0eddc61
Jason Nutt <jnutt367@gmail.com>
2021-06-08 21:00:52 -0400

    commit for build and deploy

---
+++ .next/static/tpYpEAJ6ouybDCwATqCYC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8c02c52185a6017f0aa1.css","static/chunks/pages/index-a7ffe5befcaf6e62017d.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
